76a33cdf231d9408b6165acb54401dbc5ab4f38e
João Miguel Tabosa Vaz Marques Silva <joao@miguel.ms>
2021-04-06 18:10:41 +0100

    Trying to implement some json classes

--- src/defaultr/Transform.ts
+++ src/defaultr/Transform.ts
@@ -1,21 +1,46 @@
 import { Transform } from "../Transform";
 import { SpecDriven } from "../SpecDriven";
+import { JObject } from "../JObject";
+import { Key } from "./Key";
+import { WildCards } from "./WildCards";
 
 export class Defaultr implements SpecDriven, Transform {
 
-
+    private readonly _mapRoot: Key;
+    private readonly _arrayRoot: Key | undefined;
 
     constructor(spec: Object) {
 
-        var rootString = "root";
+        var rootString = SpecDriven.RootKey;
 
+        {
+            var rootSpec = new JObject();
+            rootSpec[rootString] = spec;
+            this._mapRoot = Key.ParseSpec(rootSpec).values().next().value;
+        }
 
-        var rootSpec = new Object();
-
+        {
+            var rootSpec = new JObject();
+            rootSpec[rootString + WildCards.ARRAY] = spec;
+            var tempKey: Key;
+            try {
+                tempKey = Key.ParseSpec(rootSpec).values().next().value;
+                this._arrayRoot = tempKey;
+            }
+            catch (FormatException) {
+                // this is fine, it means the top level spec has non numeric keys
+                //  if someone passes a top level array as input later we will error then
+            }
+        }
 
     }
 
-    transform(input: Object): Object {
+    transform(input?: Object): Object {
+
+        if (input == undefined) input = new JObject();
+
+
+
 
         return new Object();
 
